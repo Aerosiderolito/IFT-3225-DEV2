20681e8eb13f685490d4521490488c5d6e22fa57
naimomo <mohammadnaim12@gmail.com>
2021-07-05 02:04:58 -0400

    random

--- Board.js
+++ Board.js
@@ -34,17 +34,33 @@ class Board extends React.Component {
     }
 
     setRandom2(tabu){
+      let randomC = Math.floor(Math.random()*tabu.length);
       
-      for(let i=0; i<2;i++){
-        let randomC = Math.floor(Math.random()*tabu.length);
+      for(let i = 0; i<2; i++){
         let coin = (Math.random());
-        if(coin>0.5 && tabu[randomC]==0){
-          tabu[randomC] = 2;
-        }else{
-          tabu[randomC] = 4;
+
+        if(coin>0.5){
+          // numero 2
+          if(tabu[randomC]==0){
+            tabu[randomC] = 2;
           }
-      }        
-  }
+          else{
+            randomC = Math.floor(Math.random()*this.state.dimension);
+            i--;
+          }
+        }
+        else{
+          // numero 4
+          if(tabu[randomC]==0){
+            tabu[randomC] = 4;
+          }
+          else{
+            randomC = Math.floor(Math.random()*this.state.dimension);
+            i--;
+          }
+        }              
+      }
+    }
 
     printer(tabu){
       
